e49a2c8db428c491920d8d734a292f9fa843b07e
tusharshende <tush2691@gmail.com>
2016-04-05 14:49:21 +0530

    Added files via upload
    only imported library.

---
+++ biogas digester.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 29 23:18:47 2016
+
+@author: Tushar
+"""
+
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+
+
+
+
+    
+    
+    
